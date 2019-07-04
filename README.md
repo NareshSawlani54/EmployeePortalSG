2a0ec62441a213d97ac3d1dc18a259a11638cbf1
naresh.sawlani <naresh.sawlani@SYNBLRDT5F4ZQG2.synechron.com>
2019-07-04 17:20:22 +0530

    Initial Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # EmployeePortalSG
 Employee Portal For Societe Generale
+
+Build Steps :
+
+1. Import as Existing Gradle Project
+2. Gradle Sync & Build
+3. Run EmployeePortalMain as a normal Java Application [Default uses port 8080]
---
+++ src/main/java/com/employeeportal/EmployeePortalMain.java
@@ -0,0 +1,13 @@
+package com.employeeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePortalMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePortalMain.class, args);
+	}
+
+}
